813aeb3d1c64446a0ced0307f7c591008160e88e
Michał Kuc <38427529+mkkuc@users.noreply.github.com>
2019-05-11 13:52:48 +0200

    baza danych utworzona, dodano modele, migracje, connectionstringi w app.config

---
+++ DataTransferObjects/Models/Priority.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTransferObjects.Models
+{
+    [Table("Priority")]
+    public class Priority
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PriorityID { get; set; }
+
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+    }
+}
---
+++ DataTransferObjects/Models/Queue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTransferObjects.Models
+{
+    [Table("Queue")]
+    public class Queue
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int QueueID { get; set; }
+
+        [ForeignKey("Group")]
+        public int GroupID { get; set; }
+
+        public virtual Group Group { get; set; }
+
+        [ForeignKey("Account")]
+        public int AccountID { get; set; }
+
+        public virtual Account Account { get; set; }
+    }
+}
