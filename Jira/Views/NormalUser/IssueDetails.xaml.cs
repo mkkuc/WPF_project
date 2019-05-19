c009f9111751300806f0f26fed3463aeac54dfae
mateuszjanel <janel.mateusz@gmail.com>
2019-05-19 15:54:06 +0200

    Grouplist view for normal user window and Issue Details window created.

---
+++ Jira/Views/NormalUser/IssueDetails.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Jira.Views.NormalUser
+{
+    /// <summary>
+    /// Logika interakcji dla klasy IssueDetails.xaml
+    /// </summary>
+    public partial class IssueDetails : Window
+    {
+        public IssueDetails()
+        {
+            InitializeComponent();
+        }
+    }
+}
