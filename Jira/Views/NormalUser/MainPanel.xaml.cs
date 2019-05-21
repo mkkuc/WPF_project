e72431ff5ac476c73dfedbb0ba751abda74e7ded
mateuszjanel <janel.mateusz@gmail.com>
2019-05-21 14:07:11 +0200

    Improvements of normal user's views.

--- Jira/Views/NormalUser/MainPanel.xaml.cs
+++ Jira/Views/NormalUser/MainPanel.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,20 @@ namespace Jira.Views.NormalUser
     /// </summary>
     public partial class MainPanel : Window
     {
+        ObservableCollection<DataTransferObjects.Models.Issue> issues = new ObservableCollection<DataTransferObjects.Models.Issue>();
         public MainPanel()
         {
             InitializeComponent();
+
+            issues.Add(new DataTransferObjects.Models.Issue
+            {
+                IssueID = 0,
+                Title = "Testowe zadanie 1",
+                Description = "Opis zadania testowego numer 1",
+                PriorityID = 0,
+                Priority = new DataTransferObjects.Models.Priority { PriorityID = 0, Name = "Ważne" }
+            });
+
         }
     }
 }
